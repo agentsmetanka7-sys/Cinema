2fb6ebf4ffbe0a3cd93173b6330454a715a081fb
agentsmetanka7-sys <agentsmetanka7@gmail.com>
2026-04-21 17:59:51 +0300

    Delete cinema-main/cinema directory

--- cinema-main/cinema/views/404.php
+++
@@ -1,5 +0,0 @@
-<section class="container py-5 text-center">
-  <h1 class="display-4 fw-bold mb-3">404</h1>
-  <p class="lead mb-4">Сторінку не знайдено</p>
-  <a class="btn btn-danger" href="/">На головну</a>
-</section>
